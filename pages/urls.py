87b14f6841eb3ade846dba71c3ea4c12106a472e
mhmdheydarii <heidarimohammadhosin@gmail.com>
2025-10-18 00:12:07 -0700

    creat pages app templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'pages'
 
 urlpatterns = [
-    path(),
+    path('', index, name = 'index'),
+    path('about/' , about, name = 'about'),
+    path('contact/', contact, name = 'contact'),
+    path('catgory/', catgory, name = 'catgory'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'index.html')
 
 
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def catgory(request):
+    return render(request, 'catgory.html')
